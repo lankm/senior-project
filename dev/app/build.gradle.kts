cd4046494f174044a33366ecc95fd75f5a0660f4
Jacob Holz <jakeholz03@gmail.com>
2023-12-06 16:22:26 -0600

    Oops old version

--- dev/app/build.gradle.kts
+++ dev/app/build.gradle.kts
@@ -67,7 +67,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-    implementation("io.coil-kt:coil:2.0.0") // Simplifies Image loading
+    implementation("io.coil-kt:coil:2.5.0") // Simplifies Image loading
     implementation("io.coil-kt:coil-compose:2.5.0") // JPC integration for coil
     implementation("androidx.navigation:navigation-compose:2.7.4") // navigation
     implementation("androidx.compose.material:material") // general UI components
