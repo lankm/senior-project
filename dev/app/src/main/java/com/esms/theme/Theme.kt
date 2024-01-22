b31b389599e47766fe96e3e39ae774a405fa2bcb
Jacob Holz <jakeholz03@gmail.com>
2024-01-22 11:44:01 -0600

    Attempted Corruption Fix
    I don't think it quite works, but it is a step in the right direction

--- dev/app/src/main/java/com/esms/theme/Theme.kt
+++ dev/app/src/main/java/com/esms/theme/Theme.kt
@@ -62,15 +62,15 @@ fun EsmsTheme(
 
     MaterialTheme(
         colors = when (theme) {
-            "Dark" -> {
-                activity?.window?.statusBarColor = darkColors.surface.toArgb()
-                darkColors }
-            "Light" -> {
-            activity?.window?.statusBarColor = lightColors.surface.toArgb()
-            lightColors }
-            else -> {
+            "Custom" -> {
                 activity?.window?.statusBarColor = params.getCustomColors().surface.toArgb()
                 params.getCustomColors() }
+            "Light" -> {
+                activity?.window?.statusBarColor = lightColors.surface.toArgb()
+                lightColors }
+            else -> {
+                activity?.window?.statusBarColor = darkColors.surface.toArgb()
+                darkColors }
         },
         typography = Typography,
         content = content
